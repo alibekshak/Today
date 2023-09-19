e78a46e9fbe650bb5a0d282e1e20b096fb4fa217
alibekshak <alibekshakiro1@gmail.com>
2023-09-19 14:03:21 +0600

    add_identifiable

--- Today/Model/Reminder.swift
+++ Today/Model/Reminder.swift
@@ -1,7 +1,8 @@
 import Foundation
 
 
-struct Reminder{
+struct Reminder: Identifiable{
+    var id: String = UUID().uuidString
     var title: String
     var dueDate: Date
     var notes: String? = nil
