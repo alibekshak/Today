8e9b2967cb39b7965a1f97a82d1bf9f12c412439
alibekshak <alibekshakiro1@gmail.com>
2023-09-27 10:21:14 +0600

    editing_reminders_save

--- Today/DetailViewController/ReminderViewController+Cell.swift
+++ Today/DetailViewController/ReminderViewController+Cell.swift
@@ -24,6 +24,9 @@ extension ReminderViewController {
     {
         var contentConfiguration = cell.textFieldConfiguration()
         contentConfiguration.text = title
+        contentConfiguration.onChange = { [weak self] title in
+            self?.workingReminder.title = title
+        }
         return contentConfiguration
     }
     
@@ -32,6 +35,9 @@ extension ReminderViewController {
     {
         var contentConfiguration = cell.datePickerConfiguration()
         contentConfiguration.date = date
+        contentConfiguration.onChange = { [weak self] dueDate in
+            self?.workingReminder.dueDate = dueDate
+       }
         return contentConfiguration
     }
 
@@ -41,6 +47,9 @@ extension ReminderViewController {
     {
         var contentConfiguration = cell.textViewConfiguration()
         contentConfiguration.text = notes
+        contentConfiguration.onChange = { [weak self] notes in
+            self?.workingReminder.notes = notes
+       }
         return contentConfiguration
     }
 
