c4cda7207091d3f22d78b251130d552b4d8759ad
alibekshak <alibekshakiro1@gmail.com>
2023-10-13 15:38:35 +0600

    problem with UIDiffableDataSource

--- Today/DetailViewController/ReminderViewController.swift
+++ Today/DetailViewController/ReminderViewController.swift
@@ -44,6 +44,7 @@ class ReminderViewController: UICollectionViewController{
         navigationItem.title = NSLocalizedString("Reminder", comment: "Reminder view controller title")
         navigationItem.rightBarButtonItem = editButtonItem
         
+        
         updateSnapshotForViewing()
     }
     
