346ab8d76d79a86eda8f1af6377636bde5255be4
alibekshak <alibekshakiro1@gmail.com>
2023-10-27 14:55:41 +0600

    Creating TodayError

---
+++ Today/Model/EKEventStore+AsyncFetch.swift
@@ -0,0 +1,23 @@
+//
+//  EKEventStore+AsyncFetch.swift
+//  Today
+//
+//  Created by Apple on 27.10.2023.
+//
+
+import Foundation
+import EventKit
+
+extension EKEventStore {
+    func reminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
+        try await withCheckedThrowingContinuation { continuation in
+            fetchReminders(matching: predicate) { reminders in
+                if let reminders {
+                    continuation.resume(returning: reminders)
+                } else {
+                    continuation.resume(throwing: TodayError.failedReadingReminders)
+                }
+            }
+        }
+    }
+}
